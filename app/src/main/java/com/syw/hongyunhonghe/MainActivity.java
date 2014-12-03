0edff65e0ec738b94b5171fc0c5a462b3f6f613d
hacker94 <87306223@163.com>
2014-12-04 02:49:48 +0800

    Build a basic main page.  Add some data to server.

--- app/src/main/java/com/syw/hongyunhonghe/MainActivity.java
+++ app/src/main/java/com/syw/hongyunhonghe/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.syw.hongyunhonghe.model.DataModel;
+
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init DataModel
+        DataModel.init(this);
+
+        // refresh magazines list
+        refreshMagazines();
+    }
+
+    private void refreshMagazines() {
+
     }
 
 
